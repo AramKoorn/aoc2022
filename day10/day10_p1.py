1fecc6358d85818cbfe241567c84d86055095b29
Aram Koorn <aram.koorn@nielsen.com>
2022-12-10 20:10:09 +0100

    day10

---
+++ day10/day10_p1.py
@@ -0,0 +1,33 @@
+f = open("input.txt", "r")
+lines = f.readlines()
+lines = [x.strip('\n').split(" ") for x in lines]
+
+cnt = 0
+v = 1
+prev = 0
+targets = [20, 60, 100, 140, 180, 220]
+signal = []
+
+
+def update_cycle(n, cnt):
+    for _ in range(1, n + 1):
+        cnt += 1
+        if cnt in targets:
+            # print((v) * cnt)
+            signal.append(v * cnt)
+            # print(v)
+    return cnt
+
+
+for x in lines:
+    v += prev
+    if v == 19:
+        b = 1
+    if x[0] == "noop":
+        prev = 0
+        cnt = update_cycle(1, cnt)
+    if x[0] == "addx":
+        prev = int(x[1])
+        cnt = update_cycle(2, cnt)
+
+print(sum(signal))
---
+++ day10/day10_p2.py
@@ -0,0 +1,40 @@
+f = open("input.txt", "r")
+lines = f.readlines()
+lines = [x.strip('\n').split(" ") for x in lines]
+
+cnt = 0
+crt = ""
+v = 1
+prev = 0
+sprite = 0
+
+
+def update_cycle(n, cnt, crt):
+    for _ in range(1, n + 1):
+        cnt += 1
+        if cnt == 41:
+            cnt -= 40
+        sprite_range = list(range(v, v + 3))
+        if cnt in sprite_range:
+            crt += "#"
+        else:
+            crt += "."
+    return cnt, crt
+
+
+for x in lines:
+    v += prev
+    if v == 19:
+        b = 1
+    # print(v)
+    if x[0] == "noop":
+        prev = 0
+        cnt, crt = update_cycle(1, cnt, crt)
+    if x[0] == "addx":
+        prev = int(x[1])
+        cnt, crt = update_cycle(2, cnt, crt)
+
+p = [x for x in crt]
+p = [p[(i - 1) * 40 : i * 40] for i in range(1, 7)]
+for x in p:
+    print(x)
