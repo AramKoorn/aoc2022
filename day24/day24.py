71d968dd287430e6fa853cf092fb0495b324b4b0
Aram Koorn <aram.koorn@nielsen.com>
2023-01-03 21:28:37 +0100

    works on test data

---
+++ day24/day24.py
@@ -0,0 +1,132 @@
+from collections import deque, defaultdict
+from copy import deepcopy
+
+
+f = open("input_test.txt", "r")
+lines = f.readlines()
+lines = [l.split("\n") for l in lines]
+
+
+grid = []
+for x in lines:
+    grid.append([x for x in x[0]])
+
+
+n = len(grid)
+m = len(grid[0])
+
+
+hor = m - 2
+vert = n - 2
+
+h_blizz = defaultdict(set)
+v_blizz = defaultdict(set)
+
+blizzards = defaultdict(set)
+
+
+def plot_grid(idx):
+    grid = [["." for x in range(m)] for x in range(n)]
+    for i in range(n):
+        for j in range(m):
+            if i == 0 or i == n - 1:
+                grid[i][j] = "#"
+            if j == 0 or j == m - 1:
+                grid[i][j] = "#"
+
+    for d, cords in blizzards[idx]:
+        i, j = cords
+        if grid[i][j] == ".":
+            grid[i][j] = d
+        else:
+            try:
+                grid[i][j] += 1
+            except:
+                grid[i][j] = 2
+
+    import numpy as np
+    print(np.array(grid))
+
+
+# initialize
+for i in range(n):
+    for j in range(m):
+        if grid[i][j] == ">" or grid[i][j] == "<" or grid[i][j] == "v" or grid[i][j] == "^":
+            blizzards[0].add((grid[i][j], (i, j)))
+
+
+for i in range(max(vert, hor)):
+    if blizzards[i] == set():
+        for d, cords in blizzards[i - 1]:
+            x, y = cords
+            if d == "<":
+                if grid[x][y - 1] != "#":
+                    blizzards[i].add((d, (x, y - 1)))
+                else:
+                    blizzards[i].add((d, (x, hor)))
+            if d == ">":
+                if grid[x][y + 1] != "#":
+                    blizzards[i].add((d, (x, y + 1)))
+                else:
+                    blizzards[i].add((d, (x, 1)))
+            if d == "v":
+                if grid[x + 1][y] != "#":
+                    blizzards[i].add((d, (x + 1, y)))
+                else:
+                    blizzards[i].add((d, (1, y)))
+            if d == "^":
+                if grid[x - 1][y] != "#":
+                    blizzards[i].add((d, (x - 1, y)))
+                else:
+                    blizzards[i].add((d, (vert, y)))
+
+
+for k, v in blizzards.items():
+    for d, cords in v:
+        if d == "<" or d == ">":
+            h_blizz[k].add(cords)
+        else:
+            v_blizz[k].add(cords)
+
+
+# BFS
+start = (0, grid[0].index("."))
+end = (n - 1, grid[n - 1].index("."))
+stack = deque([(0, start)])
+offset = ((0, 1), (1, 0), (-1, 0), (0, -1))
+visited = {(0, start)}
+
+
+while stack:
+    cnt, cords = stack.popleft()
+    x, y = cords
+    print(cnt, cords)
+
+    # position of blizzards at time cnt
+    blizz = frozenset(h_blizz[(cnt + 1) % hor] | v_blizz[(cnt + 1) % vert])
+    if (x, y) == end:
+        print(cnt)
+        break
+
+    wait = True
+    for dx, dy in offset:
+        xnew = x + dx
+        ynew = y + dy
+
+        if xnew == 2 and ynew == 1:
+            test =  2
+        # check if inside the grid
+        if x == 1 and y == 1:
+            test = 2
+
+        if grid[xnew][ynew] != "#" and (blizz, (xnew, ynew)) not in visited and (xnew, ynew) not in blizz:
+            wait = False
+            stack.append([cnt + 1, (xnew, ynew)])
+            visited.add((blizz, (xnew, ynew)))
+    if wait:
+        stack.append([cnt + 1, (x, y)])
+        visited.add((blizz, (x, y)))
+
+
+v = 1
+
